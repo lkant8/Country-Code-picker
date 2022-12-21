9141f798a1fb109dc32d2ad713367fb4145111b7
Laxmi kant <laxmipalkant01@gmail.com>
2022-12-21 22:35:17 +0530

    add CPBottomSheet.kt

---
+++ ccp/src/main/java/com/mcode/ccp/component/CPBottomSheet.kt
@@ -0,0 +1,113 @@
+package com.mcode.ccp.component
+
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Text
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.mcode.ccp.utils.Country
+import com.mcode.ccp.utils.countryList
+import com.mcode.ccp.utils.localeToEmoji
+import com.mcode.ccp.utils.searchCountryList
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CountryPickerBottomSheet(
+    title: @Composable () -> Unit,
+    show: Boolean,
+    onItemSelected: (country: Country) -> Unit,
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val countries = remember { countryList(context) }
+    var selectedCountry by remember { mutableStateOf(countries[0]) }
+    var searchValue by remember { mutableStateOf("") }
+
+    val modalBottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden
+    )
+
+    LaunchedEffect(key1 = show) {
+        if (show) modalBottomSheetState.show()
+        else modalBottomSheetState.hide()
+    }
+
+    LaunchedEffect(key1 = modalBottomSheetState.currentValue) {
+        Log.e("State updates", "Current " + modalBottomSheetState.currentValue)
+
+        if (modalBottomSheetState.currentValue == ModalBottomSheetValue.Hidden) {
+            onDismissRequest()
+        }
+    }
+    ModalBottomSheetLayout(
+        sheetState = modalBottomSheetState,
+        sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        sheetContent = {
+            title()
+
+            Column {
+                searchValue = countrySearchView(modalBottomSheetState)
+
+                LazyColumn(
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(
+                        if (searchValue.isEmpty()) {
+                            countries
+                        } else {
+                            countries.searchCountryList(searchValue)
+                        }
+                    ) { country ->
+                        Row(modifier = Modifier
+                            .clickable {
+                                selectedCountry = country
+                                onItemSelected(selectedCountry)
+                            }
+                            .padding(12.dp)) {
+                            Text(text = localeToEmoji(country.code))
+                            Text(
+                                text = country.name,
+                                modifier = Modifier
+                                    .padding(start = 8.dp)
+                                    .weight(2f)
+                            )
+                            Text(
+                                text = country.dial_code,
+                                modifier = Modifier
+                                    .padding(start = 8.dp)
+                            )
+                        }
+                        Divider(
+                            color = Color.LightGray, thickness = 0.5.dp
+                        )
+                    }
+                }
+            }
+
+        }
+    ) {
+        content()
+    }
+}
